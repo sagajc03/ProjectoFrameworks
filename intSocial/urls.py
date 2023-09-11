650a17903ade3e8f201492dfa4f764b20181cbac
sagajc03 <sagajc03@gmail.com>
2023-09-11 10:29:20 -0600

    Añadidos urls y views sin nada importante dentro

--- intSocial/urls.py
+++ intSocial/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('/login', views.login),
+    path('/signup', views.signup),
+    path('/TimeLine', views.time_line),
+    path('/post', views.post),
+    path('/newpost', views.new_post),
+    path('/profile', views.profile)
 ]
--- intSocial/views.py
+++ intSocial/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>index</h1>")
+
+def login(request):
+    return HttpResponse("<h1>login</h1>")
+
+def signup(request):
+    return HttpResponse("<h1>Sign up</h1>")
+
+def time_line(request):
+    return HttpResponse("<h1>Time Line</h1>")
+
+def post(request):
+    return HttpResponse("<h1>post</h1>")
+
+def new_post(request):
+    return HttpResponse("<h1>New post</h1>")
+
+def profile(request):
+    return HttpResponse("<h1>Perfil</h1>")
