81410e69a9cef9105f9ca9981db9e595c1b5fd08
Juan Salas <sagajc03@gmail.com>
2023-09-10 20:37:41 -0600

    Modelos de las tables de la base de datos

--- intSocial/models.py
+++ intSocial/models.py
@@ -1,2 +1,122 @@
 from django.db import models
+
+
 # Create your models here.
+class Usuario(models.Model):
+    """
+    Datos necesarios de un usuario
+    """
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=255)
+    contasenia = models.CharField(max_length=60)
+    esta_Activo = models.BooleanField(default=True)
+    es_Admin = models.BooleanField(default=False)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    def __Str__(self):
+        return self.username
+
+
+class Level(models.Model):
+    """
+    Nivel de privaciodad
+    """
+    name = models.CharField(max_length=50)
+
+
+class Profile(models.Model):
+    """
+    Datos extras que tiene un usuario
+    """
+    fecha_nacimiento = models.DateField()
+    genero = models.CharField(max_length=10)
+    imagen = models.CharField(max_length=255)
+    imagen_header = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
+    info_contacto = models.TextField()
+    email_publico = models.CharField(max_length=255)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+
+
+class Portafolio(models.Model):
+    """
+    También se les podrian considerar albums
+    """
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+class Imagen(models.Model):
+    """
+    Datos para almacenar y hacer refencias a imagenes
+    """
+    src = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    portafolio = models.ForeignKey(Portafolio, on_delete=models.SET_NULL, null=True, blank=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.TextField()
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    receptor_type = models.IntegerField()
+    post_type = models.IntegerField(default=1)
+
+
+class PostImagen(models.Model):
+    """Muchos a muchos (Post-Imagen)"""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE)
+
+
+class Likes(models.Model):
+    """
+    Likes y dislikes
+    """
+    valor = models.IntegerField()  # 1 like, 2 dislike
+    ref = models.ForeignKey(Post, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+class Comentario(models.Model):
+    """
+    Para hacer comentarios
+    """
+    ref = models.ForeignKey(Post, on_delete=models.CASCADE)  # post en el que se encuentra
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    respuesta_a = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='respuestas')
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+class Notificaciones(models.Model):
+    not_type = models.IntegerField()  # 1 likes, 2 comentarios
+    ref = models.ForeignKey(Post, on_delete=models.CASCADE)
+    receptor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='notificaciones_recibe')
+    sender = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='notificaciones_envia')
+    fue_leido = models.BooleanField(default=False)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+class Grupos(models.Model):
+    imagen = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    user = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.IntegerField()  # 1 open, 2 closed
+    creado_en = models.DateTimeField(auto_now_add=True)
