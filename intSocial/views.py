f74a6d761a4aada1ffd3c744455efd3397bea1b5
Javier Valencia <valenciavazquez5@gmail.com>
2023-09-12 09:02:05 -0600

    aweno_sisivavava

--- intSocial/views.py
+++ intSocial/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import Usuario
+from .forms import CrearNuevoUsuario
 # Create your views here.
+
 def index(request):
-    return HttpResponse("<h1>index</h1>")
+    title= 'Soy el inicio awebito'
+    return render(request, "index.html",{
+        'title': title
+    })
+
+
+def usuario(request):
+    usuarios = Usuario.objects.all()
+    return render(request, "usuario.html",{
+        'usuarios': usuarios
+    })
+    
+
+def crear_usuario(request):
+    if request.method == 'GET':
+        return render(request, 'crear_usuario.html', {
+            'form': CrearNuevoUsuario
+        })
+    else:
+        Usuario.objects.create(
+            nombre=request.POST['nombre'],
+            apellidos=request.POST['apellidos'],
+            username=request.POST['username'],
+            email=request.POST['email'],
+            contasenia=request.POST['contasenia'])
+        return render(request, 'crear_usuario.html',{
+            'form': CrearNuevoUsuario
+        })
+        
